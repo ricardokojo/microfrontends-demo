563f024f778025afc45898e8a1866ac42a350ceb
Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>
2021-11-10 11:52:02 -0300

    update: move sidemenu from fragment to container
    Signed-off-by: Ricardo H H Kojo <ricardo.kojo.dev@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 const mainContent = document.getElementById('main-content');
 
 window.addEventListener("hashchange", () => {
-    if (window.location.hash.includes('graphs')) {
+    if (window.location.hash.includes('react')) {
         mainContent.src = 'https://microfrontends-demo-2.netlify.app/';
-    } else if (window.location.hash.includes('users')) {
+    } else if (window.location.hash.includes('vue')) {
         mainContent.src = 'https://microfrontends-demo-3.netlify.app/';
     } else {
         mainContent.src = 'https://microfrontends-demo-1.netlify.app/';
